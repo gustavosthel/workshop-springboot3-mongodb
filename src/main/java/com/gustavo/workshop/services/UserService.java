bcffa38a823af50f856caf6b85bcc2b6914325b0
Gustavo Sthel <gustavosthel@gmail.com>
2024-03-27 18:15:56 -0300

    Inserção de usuário com POST

--- src/main/java/com/gustavo/workshop/services/UserService.java
+++ src/main/java/com/gustavo/workshop/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavo.workshop.domain.User;
+import com.gustavo.workshop.dto.UserDTO;
 import com.gustavo.workshop.repository.UserRepository;
 import com.gustavo.workshop.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail()); 
 	}
 }
